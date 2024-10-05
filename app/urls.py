2907921bf08fb61bd9992bdbc97e26e6d4942686
DanielPRech7 <daniel.prech19@gmail.com>
2024-10-05 13:48:55 -0300

    Subindo projeto Django1

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_create_list_view, genre_detail_view
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_create_list_view, name='genre-create-list'),  # Listar e criar
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),  # Detalhar, atualizar e deletar
+
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),  # Listar e criar
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),  # Detalhar, atualizar e deletar
 ]
